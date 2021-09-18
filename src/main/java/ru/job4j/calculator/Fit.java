7669438b2808c00c157b45b6cbb4662d59636a70
evgen32br <4ibo32@mail.ru>
2021-09-18 19:29:12 +0300

    6. Идеальный вес [#41584]

---
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -0,0 +1,23 @@
+package ru.job4j.calculator;
+
+public class Fit {
+    public static double manWeight(short height) {
+        return (height - 100) * 1.15;
+    }
+
+    public static double womanWeight(short height) {
+        return (height - 110) * 1.15;
+    }
+
+    public static void main(String[] args) {
+        short height = 178;  // мой рост
+        double man = Fit.manWeight(height);
+        double woman = Fit.womanWeight(height);
+
+        System.out.print("Man " + height + " is ");
+        System.out.format("%.1f", man);
+        System.out.println();
+        System.out.print("Woman " + height + " is ");
+        System.out.format("%.1f", woman);
+    }
+}
